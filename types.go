2241ed17543b36c5eb46cdbd07ec3b147775a606
Hedingber <hedingber@gmail.com>
2019-11-03 21:42:54 +0200

    Supporting multiple metrics + new Scaler version (#2)

--- types.go
+++ types.go
@@ -1,26 +1,22 @@
 package scaler_types
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
 
 type AutoScalerOptions struct {
 	Namespace     string
 	ScaleInterval time.Duration
-	ScaleWindow   time.Duration
-	MetricName    string
-	Threshold     int64
-}
-
-type PollerOptions struct {
-	MetricInterval time.Duration
-	MetricName     string
-	Namespace      string
-	GroupKind      string
+	GroupKind     string
 }
 
 type ResourceScalerConfig struct {
 	KubeconfigPath    string
 	AutoScalerOptions AutoScalerOptions
-	PollerOptions     PollerOptions
 	DLXOptions        DLXOptions
 }
 
@@ -38,4 +34,73 @@ type ResourceScaler interface {
 	GetConfig() (*ResourceScalerConfig, error)
 }
 
-type Resource string
+type Resource struct {
+	Name               string          `json:"name,omitempty"`
+	ScaleResources     []ScaleResource `json:"scale_resources,omitempty"`
+	LastScaleEvent     ScaleEvent      `json:"last_scale_event,omitempty"`
+	LastScaleEventTime time.Time       `json:"last_scale_event_time,omitempty"`
+}
+
+func (r Resource) String() string {
+	out, err := json.Marshal(r)
+	if err != nil {
+		panic(err)
+	}
+	return string(out)
+}
+
+type ScaleResource struct {
+	MetricName string        `json:"metric_name,omitempty"`
+	WindowSize time.Duration `json:"windows_size,omitempty"`
+	Threshold  int           `json:"threshold,omitempty"`
+}
+
+func (sr ScaleResource) GetKubernetesMetricName() string {
+	return fmt.Sprintf("%s_per_%s", sr.MetricName, shortDurationString(sr.WindowSize))
+}
+
+func shortDurationString(d time.Duration) string {
+	s := d.String()
+	if strings.HasSuffix(s, "m0s") {
+		s = s[:len(s)-2]
+	}
+	if strings.HasSuffix(s, "h0m") {
+		s = s[:len(s)-2]
+	}
+	return s
+}
+
+func (sr ScaleResource) String() string {
+	out, err := json.Marshal(sr)
+	if err != nil {
+		panic(err)
+	}
+	return string(out)
+}
+
+type ScaleEvent string
+
+const (
+	NonScaleEvent                    ScaleEvent = "non"
+	ScaleFromZeroStartedScaleEvent   ScaleEvent = "scaleFromZeroStarted"
+	ScaleFromZeroCompletedScaleEvent ScaleEvent = "scaleFromZeroCompleted"
+	ScaleToZeroStartedScaleEvent     ScaleEvent = "scaleToZeroStarted"
+	ScaleToZeroCompletedScaleEvent   ScaleEvent = "scaleToZeroCompleted"
+)
+
+func ParseScaleEvent(scaleEventStr string) (ScaleEvent, error) {
+	switch scaleEventStr {
+	case string(NonScaleEvent):
+		return NonScaleEvent, nil
+	case string(ScaleFromZeroStartedScaleEvent):
+		return ScaleFromZeroStartedScaleEvent, nil
+	case string(ScaleFromZeroCompletedScaleEvent):
+		return ScaleFromZeroCompletedScaleEvent, nil
+	case string(ScaleToZeroStartedScaleEvent):
+		return ScaleToZeroStartedScaleEvent, nil
+	case string(ScaleToZeroCompletedScaleEvent):
+		return ScaleToZeroCompletedScaleEvent, nil
+	default:
+		return "", errors.New(fmt.Sprintf("Unknown scale event: %s", scaleEventStr))
+	}
+}
