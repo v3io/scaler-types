1d8f53f8d693f313837d8e7619fb3374b441b1c4
Hedingber <hedingber@gmail.com>
2019-12-29 15:27:10 +0200

    Pretty print time.duration (#7)

--- types.go
+++ types.go
@@ -10,7 +10,7 @@ import (
 
 type AutoScalerOptions struct {
 	Namespace     string
-	ScaleInterval time.Duration
+	ScaleInterval Duration
 	GroupKind     string
 }
 
@@ -26,7 +26,7 @@ type DLXOptions struct {
 	TargetPathHeader         string
 	TargetPort               int
 	ListenAddress            string
-	ResourceReadinessTimeout time.Duration
+	ResourceReadinessTimeout Duration
 }
 
 type ResourceScaler interface {
@@ -51,16 +51,16 @@ func (r Resource) String() string {
 }
 
 type ScaleResource struct {
-	MetricName string        `json:"metric_name,omitempty"`
-	WindowSize time.Duration `json:"windows_size,omitempty"`
-	Threshold  int           `json:"threshold,omitempty"`
+	MetricName string   `json:"metric_name,omitempty"`
+	WindowSize Duration `json:"windows_size,omitempty"`
+	Threshold  int      `json:"threshold,omitempty"`
 }
 
 func (sr ScaleResource) GetKubernetesMetricName() string {
 	return fmt.Sprintf("%s_per_%s", sr.MetricName, shortDurationString(sr.WindowSize))
 }
 
-func shortDurationString(d time.Duration) string {
+func shortDurationString(d Duration) string {
 	s := d.String()
 	if strings.HasSuffix(s, "m0s") {
 		s = s[:len(s)-2]
@@ -103,5 +103,34 @@ func ParseScaleEvent(scaleEventStr string) (ScaleEvent, error) {
 		return ScaleToZeroCompletedScaleEvent, nil
 	default:
 		return "", errors.New(fmt.Sprintf("Unknown scale event: %s", scaleEventStr))
+	}
+}
+
+type Duration struct {
+	time.Duration
+}
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		d.Duration = time.Duration(value)
+		return nil
+	case string:
+		var err error
+		d.Duration, err = time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		return errors.New("invalid duration")
 	}
 }
